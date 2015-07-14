9c3c263aeec515a927cd0f35a54371f3b6b8bfee
Geo Baby <geobaby94@gmail.com>
2015-07-14 18:01:23 +0530

    Initial Commit

---
+++ CompareBox/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace CompareBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Paste your URL here : ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(333, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(16, 71);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(333, 20);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            this.openFileDialog2.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog2_FileOk);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(486, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(299, 169);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
+            this.button4.Location = new System.Drawing.Point(425, 42);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(55, 49);
+            this.button4.TabIndex = 13;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(486, 230);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(299, 168);
+            this.pictureBox2.TabIndex = 14;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox2.Location = new System.Drawing.Point(16, 107);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(464, 291);
+            this.richTextBox2.TabIndex = 16;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(355, 42);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(64, 23);
+            this.progressBar2.TabIndex = 17;
+            this.progressBar2.Click += new System.EventHandler(this.progressBar2_Click);
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(355, 68);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(64, 23);
+            this.progressBar3.TabIndex = 18;
+            this.progressBar3.Click += new System.EventHandler(this.progressBar3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(188, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(821, 424);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "CompareBox";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ CompareBox/Form1.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CompareBox
+{
+    public partial class Form1 : Form
+    {
+        private string fname1 = string.Empty;
+        private string fname2 = string.Empty;
+        private int count1 = 0;
+        
+        private int count2 = 0;
+        private Bitmap img1;
+        private Bitmap img2;
+        private Bitmap buttonImage;
+        private bool flag = true;
+        private int x=0, y=0, i=0, j=0;
+        private double total = 0.0;
+        private int myProgress1 = 0, myProgress2 = 0;
+        private int count = 0;
+
+        Color firstImageColor = Color.Empty;
+        Color secondImageColor = Color.Empty;
+
+        double resultNew = 0.0, newMaxResult = 0.0, newMinResult = 0.0;
+        double oldResult = 0.0, oldMinResult = 0.0, oldMaxResult = 0.0;
+
+        int[] firstImageMatrix = new int[0];
+        int[] secondImageMatrix = new int[0];
+        int k = 0;
+        int l = 0;
+        double redTintDifference = 0.0;
+        List<int> myList1 = new List<int>();
+        List<int> myList2 = new List<int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeButton();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            progressBar2.Visible = false;
+            progressBar2.ForeColor = Color.Orange;
+            progressBar3.Visible = false;
+            progressBar3.ForeColor = Color.Orange;
+
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = false;
+            this.pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            this.pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+        private void InitializeButton()
+        {
+            //button4.Image = CompareBox.Properties.Resources.
+            buttonImage = new Bitmap(button4.Image, button4.Width, button4.Height);
+            button4.Image = buttonImage;
+            button4.BackColor = Color.Transparent;
+            button4.ForeColor = System.Drawing.Color.White;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            button4.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            button4.FlatAppearance.BorderSize = 0;
+        }
+        
+        //mouse enteres over the button
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            //change the backgroudn color of the image
+            button4.UseVisualStyleBackColor = false;
+            button4.BackColor = Color.GhostWhite;
+        }
+
+        //mouse leave after hover over
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            button4.BackColor = Color.Transparent;
+        }
+
+        //mouse clicked over the button
+        private void button1_MouseDown(object sender, MouseEventArgs e)
+        {
+            button4.BackColor = Color.Blue;   
+        }
+
+        //mouse clicked and goes up over the button ;)
+        private void button1_MouseUp(object sender, MouseEventArgs e)
+        {
+            button4.BackColor = Color.Transparent;
+        }
+        
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //convert two images pixel by pixel taking eaxh pixel's red color value.
+            /*
+             * store 1st image's each pixel's red color value in a variable of Color type.
+             * 
+             * two images can be same and diff in these aspects:
+             * 
+             * YES/NO
+             * 
+             * 1. two same-color images with same size [done]
+             * 2. two same-color images with diff size
+             * 3. two diff-color images with same size [done]
+             * 4. two diff-color images with diff size [done]
+             * 
+             * 0 -100
+             * 
+             * 1. two same-color images with same size 
+             * 2. two same-color images with diff size
+             * 3. two diff-color images with same size
+             * 4. two diff-color images with diff size 
+             * 
+             * */
+            CompareImages();  
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+            
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //richTextBox2.AppendText("\nLoading image..");
+            int a, b;
+            try
+            {
+                pictureBox1.Load(textBox1.Text);
+                pictureBox1.Visible = true;
+                //pictureBox1.ImageLocation = textBox1.Text;    //alternative
+
+                img1 = new Bitmap(pictureBox1.Image);
+                progressBar2.Visible = true;
+                progressBar2.Maximum = img1.Width;
+
+                //iterate through each pixel of first image
+                for (a = 0; a < img1.Width; a++)
+                {
+                    for (b = 0; b < img1.Height; b++)
+                    {
+                        //firstImageColor = img1.GetPixel(a, b);
+                        try
+                        {
+                            myList1.Add(img1.GetPixel(a, b).R);
+                        }
+                        catch (Exception exception)
+                        {
+                            MessageBox.Show(exception.ToString(), "Error");
+                        }
+                    }
+                    progressBar2.Value++;
+                }
+
+                richTextBox2.AppendText("\nImage loaded");
+            }
+            catch (FileNotFoundException fe)
+            {
+                richTextBox2.AppendText("\nPlease paste a valid URL");
+            }
+            catch (ArgumentException argumentE)
+            {
+                richTextBox2.AppendText("\nIncorrect URL");
+            }
+            catch (InvalidOperationException)
+            {
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            //richTextBox2.AppendText("\nLoading image..");
+            int c, d;
+            try
+            {
+                pictureBox2.Load(textBox2.Text);
+                pictureBox2.Visible = true;
+                //pictureBox2.ImageLocation = textBox2.Text;    //alternative
+
+                img2 = new Bitmap(pictureBox2.Image);
+                progressBar3.Visible = true;
+                progressBar3.Maximum = img2.Width;
+
+                //iterate through each pixel of second image
+                for (c = 0; c < img2.Width; c++)
+                {
+                    for (d = 0; d < img2.Height; d++)
+                    {
+                        //secondImageColor = img2.GetPixel(c, d);
+                        try
+                        {
+                            myList2.Add(img2.GetPixel(c, d).R);
+                        }
+                        catch (Exception exception)
+                        {
+                            MessageBox.Show(exception.ToString(), "Error");
+                        }
+                    }
+                    progressBar3.Value++;
+                }
+                richTextBox2.AppendText("\nImage loaded");
+            }
+            catch (FileNotFoundException fe)
+            {
+                richTextBox2.AppendText("\nPlease paste a valid URL");
+            }
+            catch (ArgumentException argumentE)
+            {
+                richTextBox2.AppendText("\nIncorrect URL");
+            }
+            catch (InvalidOperationException)
+            {
+            }
+        }
+
+        private void CompareImages()
+        {
+            if ((string.IsNullOrWhiteSpace(textBox1.Text)) || (string.IsNullOrWhiteSpace(textBox2.Text)))
+            {
+                MessageBox.Show("Please enter image links","Image links");
+            }
+
+            //converting our list to array
+            firstImageMatrix = myList1.ToArray();
+            secondImageMatrix = myList2.ToArray();
+
+            //next we go for comparing the red tint of each pixel of each image using mean square method
+            while ((i < firstImageMatrix.Length) && (j < secondImageMatrix.Length))
+            {
+                //perform some operations
+                redTintDifference += System.Math.Pow((firstImageMatrix[i] - secondImageMatrix[j]), 2);
+                i++;
+                j++;
+            }
+
+            string result = null;
+            try
+            {
+                double sizeMultiplied = (img1.Width * img1.Height) + (img2.Width * img2.Height);
+                result = (redTintDifference / sizeMultiplied).ToString();
+            }
+            catch (NullReferenceException ne)
+            {
+                //MessageBox.Show("Please try again","Try Again");
+                richTextBox2.AppendText("\nPlease try again");
+            }
+            
+            oldResult = Convert.ToDouble(result);
+            oldMaxResult = 10000;
+            oldMinResult = 0;
+            newMinResult = 0;
+            newMaxResult = 100;
+            resultNew = (((oldResult - oldMinResult) * (newMaxResult - newMinResult)) / (oldMaxResult - oldMinResult)) + newMinResult;
+            int resultNewInt = Convert.ToInt16(resultNew);
+            //MessageBox.Show(resultNewInt.ToString(),"Here i am");
+
+            if (!((string.IsNullOrWhiteSpace(textBox1.Text)) || (string.IsNullOrWhiteSpace(textBox2.Text))))
+            {
+                resultNewInt = 100 - resultNewInt;
+                richTextBox2.AppendText("\n\n\nResult: " + resultNewInt);
+            }
+
+            if ((resultNewInt == 100) && !((string.IsNullOrWhiteSpace(textBox1.Text)) || (string.IsNullOrWhiteSpace(textBox2.Text))) )
+            {
+                richTextBox2.AppendText("\n\nExactly same images!");
+            }
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void openFileDialog2_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+            richTextBox2.SelectionStart = richTextBox2.Text.Length;
+            richTextBox2.ScrollToCaret();
+        }
+
+        private void progressBar2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = String.Empty;
+            textBox2.Text = String.Empty;
+            richTextBox2.Text = String.Empty;
+            //progressBar2.Value = 0;
+            //progressBar3.Value = 0;
+        }
+
+    }
+}
